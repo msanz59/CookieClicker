3b52b6504686933fcd3f52eaf8ff2463d71a3e25
msanz59 <msanzpalomar@gmail.com>
2025-06-12 15:23:52 +0200

    cookie added

---
+++ CookieClicker/src/main/java/com/msanz/cookieclicker/CookieClicker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.msanz.cookieclicker;
+
+/**
+ *
+ * @author msanz
+ */
+public class CookieClicker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
